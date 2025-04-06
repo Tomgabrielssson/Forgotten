603c5d4a365e767fa9a45391b9fe512f6f76362c
Tomgabrielssson <gabrielssontom@gmail.com>
2025-04-06 15:24:57 +0100

    Update script.js
    addition of sfx setting, ability to play/pause sound/sfx sample

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function setMode(mode) {
     document.body.className = mode;
 }
 const settingsDiv = document.getElementById('settings');
-const audioSample = document.getElementById('audio-sample');
+const audioSample = document.getElementById('audioSample');
 const volumeLabel = document.getElementById('volume-label');
+const sfxSample = document.getElementById('sfx-sample');
+const sfxVolumeLabel = document.getElementById('sfx-volume-label');
 
 function toggleSettings() {
     settingsDiv.style.display = (settingsDiv.style.display === 'none' || settingsDiv.style.display === '') 
@@ -22,8 +24,12 @@ function setMode(mode) {
     document.body.className = mode;
 }
 
-function playSound() {
-    audioSample.play();
+function toggleplay() {
+    if (audioSample.paused) {
+        audioSample.play();
+    } else {
+        audioSample.pause();
+    }
 }
 
 function adjustVolume(value) {
@@ -35,4 +41,23 @@ function toggleMute() {
     audioSample.muted = !audioSample.muted;
     const buttonText = audioSample.muted ? "Unmute" : "Mute";
     document.querySelector('.sound-buttons button:last-child').textContent = buttonText;
+}
+
+function playSFX() {
+    if (sfxSample.paused) {
+        sfxSample.play();
+    } else {
+        sfxSample.pause();
+    }
+}
+
+function adjustSFXVolume(value) {
+    sfxSample.volume = value / 100;
+    sfxVolumeLabel.textContent = `${value}%`;
+}
+
+function toggleSFXMute() {
+    const muteButton = document.querySelector('.sfx-buttons button:last-child');
+    sfxSample.muted = !sfxSample.muted;
+    muteButton.textContent = sfxSample.muted ? "Unmute SFX" : "Mute SFX";
 }
